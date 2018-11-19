88f6c06826231ba7d3c4f9d6b85af543a5d0dfbe
EMCain <>
2018-11-18 21:58:51 -0800

    add stars to end of review

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 import constants as c
-from random import choice, sample
+from random import choice, sample, randint
 
 def review():
     template = choice(c.TEMPLATES)
@@ -7,7 +7,9 @@ def review():
     x, y = sample(c.INGREDIENTS, 2)
 
     sub = template.format(x, y)
-    return '{} {}'.format(sub, opinion)
+    stars = c.STAR * randint(1, 3)
+
+    return '{} {} {}'.format(sub, opinion, stars)
 
 if __name__ == '__main__':
     for i in range(0, 5):
