e27d1d22520a28bc7427b26619a564aed7663575
Hannah Stepanek <hmstepanek@gmail.com>
2020-09-25 13:53:42 -0700

    Run the scheduler in the background
    Previously, the scheduler process was not launched in the background
    which meant that the worker process was never launched:
    ```
    about to start
    got env vars
    ```
    Now, the scheduler process is launched in the background which allows
    the second worker process to launch:
    ```
    20:48:52 Worker rq:worker:38665c44297e4e7a83d931fd9102bbc9: started, version 1.4.0
    20:48:52 *** Listening on job_scheduler_queue...
    about to start
    got env vars
    ```
    Note the additional stdout message that the worker is started and
    listening.

--- tasks-start.sh
+++ tasks-start.sh
@@ -2,6 +2,8 @@
 
 cd /var/app
 
-#python app/task_queue/print_review.py
-python app/task_queue/scheduler.py
+# Since this is running multiple processes, 
+# run the first process in the background so
+# that the second process is launched.
+python app/task_queue/scheduler.py & 
 python app/task_queue/worker.py
